e7999bde1a3abd7da890114138199aa0647f398a
Sumedh Udupa U <68189818+sumedhudupa@users.noreply.github.com>
2024-03-17 14:53:31 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,8 +1,26 @@
-from django.urls import path
-from . import views
+"""
+URL configuration for file1 project.
 
-app_name = 'todolist'
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+
 urlpatterns = [
-    path("", views.index, name='index'),
-    path("add/", views.add, name='add'),
+    path('admin/', admin.site.urls),
+    path("members/", include("members.urls")),
+    path("newyear/", include("newyear.urls")),
+    path("todolist/", include("todolist.urls")),
+path("__debug__/", include("debug_toolbar.urls")),
 ]
